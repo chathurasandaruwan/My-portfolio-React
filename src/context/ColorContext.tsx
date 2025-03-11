8359d420d65e4731fe3be7b8cf7b03bc2f2c751e
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-03-11 23:03:25 +0530

    create color context

---
+++ src/context/ColorContext.tsx
@@ -0,0 +1,26 @@
+import type React from "react"
+import { createContext, useState, useContext } from "react"
+
+type Color = "white" | "green" | "yellow" | "blue"
+
+type ColorContextType = {
+    color: Color
+    setColor: (color: Color) => void
+}
+
+const ColorContext = createContext<ColorContextType | undefined>(undefined)
+
+export const ColorProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [color, setColor] = useState<Color>("white")
+
+    return <ColorContext.Provider value={{ color, setColor }}>{children}</ColorContext.Provider>
+}
+
+export const useColor = () => {
+    const context = useContext(ColorContext)
+    if (context === undefined) {
+        throw new Error("useColor must be used within a ColorProvider")
+    }
+    return context
+}
+
