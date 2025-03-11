53ae3bbfc344a8e155f525906029044412a1c1b7
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-03-11 23:58:37 +0530

    cteate some thins in home section

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,8 +11,25 @@ export function Home() {
     }[color]
 
     return (
-        <div className="container mx-auto px-4 py-16 md:pl-34" style={{ color: themeColor }}>
-            <h1>Home</h1>
-        </div>
+        <section className="relative min-h-screen">
+            <div className="container mx-auto px-4 py-16 md:pl-34" style={{color: themeColor}}>
+                <div
+                    className="relative w-32 h-32 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-primary/50 p-1 bg-background/50 backdrop-blur-sm shadow-xl ">
+                    <div
+                        className="absolute rounded-full"/>
+                    <img
+                        src="src/assets/images/my-img.jpeg"
+                        alt="Profile"
+                        className="object-cover w-full h-full rounded-full"
+                    />
+                </div>
+                <h1 className="mb-6 text-4xl font-bold md:text-6xl">My Portfolio</h1>
+                <p className="text-xl" style={{opacity: 0.8}}>
+                    Welcome to my portfolio website. Navigate using the sidebar to explore my projects and learn more
+                    about
+                    me.
+                </p>
+            </div>
+        </section>
     );
 }
