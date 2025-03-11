db2579fccf15fcf0cb549fc5d9ca522fef102afb
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-03-11 21:35:02 +0530

    create all sections component and set navigations

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Navbar from "./components/NavBar.tsx";
 import {Home} from "./components/Home.tsx";
 import {About} from "./components/About.tsx";
+import {Skills} from "./components/Skills.tsx";
+import {Education} from "./components/Education.tsx";
+import {Projects} from "./components/Projects.tsx";
+import {Contact} from "./components/Contact.tsx";
 
 function App() {
     return (
@@ -11,6 +15,10 @@ function App() {
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/about" element={<About />} />
+                        <Route path="/skills" element={<Skills />} />
+                        <Route path="/education" element={<Education />} />
+                        <Route path="/projects" element={<Projects />} />
+                        <Route path="/contact" element={<Contact />} />
                     </Routes>
                 </div>
             </Router>
