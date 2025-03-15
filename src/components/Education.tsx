f585ee28afb98b6faf45f6ff392b0683f743e3a9
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-03-15 12:44:13 +0530

    start create Education.tsx page

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,5 +1,24 @@
+import {useColor} from "../context/ColorContext.tsx";
+
 export function Education() {
+
+    const {color} = useColor()
+
+    const themeColor = {
+        white: "#E2EAF4",
+        green: "#4ade80",
+        yellow: "#facc15",
+        blue: "#60a5fa",
+    }[color]
+
     return (
-        <>education</>
+        <section className="relative min-h-screen w-full overflow-hidden md:pl-30 md:pt-20 lg:px-30 lg:pl-40 ">
+            <div
+                className="container mx-auto px-4 py-18 md:py-16"
+                style={{color: themeColor}}
+            >
+                <h1 className="mb-6 text-4xl font-bold md:text-6xl">Education</h1>
+            </div>
+        </section>
     );
 }
